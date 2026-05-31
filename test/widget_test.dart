f38e0e777804a4078d6d7993bb36b5237e2aa22e
OUFA14 <seifh0307@gmail.com>
2026-05-31 23:17:32 +0300

    Add initial test for test runner configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -28,3 +28,11 @@
 //     expect(find.text('1'), findsOneWidget);
 //   });
 // }
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('test runner is configured', () {
+    expect(true, isTrue);
+  });
+}
+
